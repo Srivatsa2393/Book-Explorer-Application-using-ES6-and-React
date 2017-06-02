2740d4437217a0cb3a10edd4ec922984984124e6
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-03 02:43:14 +0530

    creating an input component

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends React.Component{
+  search(){
+    console.log('search');
+  }
+
   render() {
     return(
       <div>
-        The Global App Component!
+        <h1>Book Explorer!</h1>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for a group"
+              />
+            <InputGroup.Addon onClick = {() => this.search()}>
+              <Glyphicon glyph="search">
+
+              </Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
       </div>
     )
   }
