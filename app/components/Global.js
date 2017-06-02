fa19a793116373571e7f7d464dac2e6bde403e9b
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-03 02:32:50 +0530

    Making a Global component

---
+++ app/components/Global.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Global extends React.Component{
+  render() {
+    return(
+      <div>
+        The Global App Component!
+      </div>
+    )
+  }
+};
+
+export default Global;
